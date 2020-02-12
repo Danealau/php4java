f95b881542a880df5dc3a3f2913f72cc54a791b9
storyboardcreativity <storyboardcreativity@gmail.com>
2020-02-12 19:42:44 +0300

    Added thread-safety for each PHP instance and any it's value

--- src/main/java/php4java/Impl/PhpInstance.java
+++ src/main/java/php4java/Impl/PhpInstance.java
@@ -2,6 +2,7 @@ package php4java.Impl;
 
 import php4java.Interfaces.*;
 import java.lang.reflect.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PhpInstance implements IPhp
 {
@@ -11,27 +12,48 @@ public class PhpInstance implements IPhp
             throw new Exception();
         
         // Try to init new PHP interpreter
+        Lock();
         obj.getClass().getDeclaredMethod("init").invoke(obj);
+        Unlock();
         _php = obj;
     }
 
     @Override
     public IPhpVal execString(String code) throws php4java.Php4JavaException
     {
+        Object result = null;
         try
         {
-            var result = _php.getClass().getDeclaredMethod("__eval", new Class[]{String.class}).invoke(_php, "eval('" + code + "');");
-            return new PhpVal(result);
+            Lock();
+            result = _php.getClass().getDeclaredMethod("__eval", new Class[]{String.class}).invoke(_php, "eval('" + code + "');");
+            Unlock();
         }
-        catch(InvocationTargetException exc)
+        catch(InvocationTargetException | NoSuchMethodException | IllegalArgumentException | IllegalAccessException exc)
         {
             throw new php4java.Php4JavaException(exc.getCause().toString());
         }
-        catch(IllegalAccessException | NoSuchMethodException exc)
+
+        try
+        {
+            return new PhpVal(this, result);
+        }
+        catch(IllegalAccessException exc)
         {
             throw new php4java.Php4JavaException(exc.toString());
         }
     }
+
+    public void Lock()
+    {
+        _mutex.lock();
+    }
+
+    public void Unlock()
+    {
+        _mutex.unlock();
+    }
+
+    protected ReentrantLock _mutex = new ReentrantLock();
 
     protected Object _php;
 }
--- src/main/java/php4java/Impl/PhpVal.java
+++ src/main/java/php4java/Impl/PhpVal.java
@@ -7,11 +7,12 @@ import php4java.Native.*;
 
 public class PhpVal implements IPhpVal
 {
-    public PhpVal(Object obj) throws IllegalAccessException
+    public PhpVal(PhpInstance parent, Object obj) throws IllegalAccessException
     {
         if (obj.getClass().getName() != "php4java.Native.Zval")
             throw new IllegalAccessException();
         _phpVal = obj;
+        _parent = parent;
     }
 
     @Override
@@ -19,7 +20,10 @@ public class PhpVal implements IPhpVal
     {
         try
         {
-            return (long)_phpVal.getClass().getDeclaredMethod("getLong").invoke(_phpVal);
+            _parent.Lock();
+            var lg = (long)_phpVal.getClass().getDeclaredMethod("getLong").invoke(_phpVal);
+            _parent.Unlock();
+            return lg;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
         return null;
@@ -30,7 +34,10 @@ public class PhpVal implements IPhpVal
     {
         try
         {
-            return (double)_phpVal.getClass().getDeclaredMethod("getDouble").invoke(_phpVal);
+            _parent.Lock();
+            var dbl = (double)_phpVal.getClass().getDeclaredMethod("getDouble").invoke(_phpVal);
+            _parent.Unlock();
+            return dbl;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
         return null;
@@ -41,7 +48,10 @@ public class PhpVal implements IPhpVal
     {
         try
         {
-            return (boolean)_phpVal.getClass().getDeclaredMethod("getBoolean").invoke(_phpVal);
+            _parent.Lock();
+            var bl = (boolean)_phpVal.getClass().getDeclaredMethod("getBoolean").invoke(_phpVal);
+            _parent.Unlock();
+            return bl;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
         return null;
@@ -52,7 +62,10 @@ public class PhpVal implements IPhpVal
     {
         try
         {
-            return (String)_phpVal.getClass().getDeclaredMethod("getString").invoke(_phpVal);
+            _parent.Lock();
+            var str = (String)_phpVal.getClass().getDeclaredMethod("getString").invoke(_phpVal);
+            _parent.Unlock();
+            return str;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
         return null;
@@ -63,10 +76,13 @@ public class PhpVal implements IPhpVal
     {
         try
         {
+            _parent.Lock();
             var res = (Object[])_phpVal.getClass().getDeclaredMethod("getArray").invoke(_phpVal);
+            _parent.Unlock();
+
             var result = new IPhpVal[res.length];
             for (int i = 0; i < res.length; ++i)
-                result[i] = new PhpVal(res[i]);
+                result[i] = new PhpVal(_parent, res[i]);
             return result;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
@@ -78,7 +94,11 @@ public class PhpVal implements IPhpVal
     {
         try
         {
+            _parent.Lock();
             var map = (Map<String, Zval>)_phpVal.getClass().getDeclaredMethod("getHash").invoke(_phpVal);
+            _parent.Unlock();
+
+            // FIXME: C-code fails if we use this map
             return null;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
@@ -90,11 +110,15 @@ public class PhpVal implements IPhpVal
     {
         try
         {
-            return (String)_phpVal.getClass().getDeclaredMethod("getString").invoke(_phpVal);
+            _parent.Lock();
+            var json = (String)_phpVal.getClass().getDeclaredMethod("getString").invoke(_phpVal);
+            _parent.Unlock();
+            return json;
         }
         catch(InvocationTargetException | IllegalAccessException | NoSuchMethodException exc) {}
         return null;
     }
 
     protected Object _phpVal;
+    protected PhpInstance _parent;
 }
